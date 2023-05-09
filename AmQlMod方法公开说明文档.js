be81d181143fd87a3b7b57422c1e0f78f31e2db5
fjwpsyb <350381204@qq.com>
2023-05-09 20:31:15 +0800

    first commit

---
+++ AmQlMod方法公开说明文档.js
@@ -0,0 +1,148 @@
+/**
+ * @author Doraemon
+ * @name AmQlMod方法公开说明文档
+ * @origin Doraemon
+ * @version 1.0.5
+ * @description AmQlMod方法公开说明文档
+ * @rule ^(test3)$
+ * @admin true
+ * @public false
+ * @priority 1000
+ */
+
+let waitObj = { wait: 10 }
+const QlMod = require("../红灯区/mod/AmQlMod")
+
+//要执行的容器序号
+let i = 0
+ //插件入口
+module.exports = async s => {
+    
+    let qlDb = await QlMod.GetQlDataBase()
+    let qlDbArr = qlDb["data"] || []
+    console.log(qlDbArr);
+    //未配置奶酪
+    if (qlDbArr.length == 0) return s.reply('请先发“面板管理”添加面板')
+
+    /* 获取青龙青龙面板版本号 */
+    console.log(JSON.stringify(await QlMod.GetQlVersion(qlDbArr, i)));
+
+    /************* 配置文件相关****************/
+    /* 获取配置文件 */
+    console.log(await QlMod.GetQlConfig(qlDbArr, i, 'config.sh'));
+
+    // /* 保存配置文件 */
+    // log(JSON.stringify(QlMod.UpdateQlConfig(qlDbArr, i, 'config.sh', '需要保存的Str')))   //慎用，注释了，运行的时候
+
+    // /************* 定时任务相关****************/
+    // /* 获取所有任务 */
+    // log(JSON.stringify(QlMod.GetQlAllCrons(qlDbArr, i)))
+
+    // /* 运行、更新、启用和禁用 
+    //  * 第三个参数 任务名字或脚本名关键词，方法内部会先读取所有任务-遍历所有任务找出包含关键词的任务
+    //  * 第四个参数 可选：stop停止 run运行 disable禁用 enable启用 pin置顶 unpin取消置顶
+    //  * 第五个参数 布尔值，false 或不填 则会调用方法内部选择器(如果有多个，否则直接返回数据)让用户选择一个，true返回找到的第一个任务
+    //  */
+    // log(JSON.stringify(QlMod.PutQlCrons(s,qlDbArr, i, 'test.js', 'stop', false)))
+
+    // /* 获取脚本状态 参数同上*/
+    // console.log(JSON.stringify(await QlMod.GetQlCronsStatus(s,qlDbArr, i, 'KingRan_KR/jd_sevenDay.js', false)));
+
+    // /* 获取任务日志 */
+    // console.log(JSON.stringify(await QlMod.GetQlCronsLogs(s, qlDbArr, i, 'KingRan_KR/jd_sevenDay.js', true)));
+
+    // /* 删除定时任务 */
+    // log(JSON.stringify(QlMod.DelQlCrons(s,qlDbArr, i, 'test.js', false)))
+
+    // /* 添加任务，传用户名参数调用后，函数内部有询问器，会询问后续流程 */
+    // log(JSON.stringify(QlMod.AddQlCrons(s,qlDbArr, i, '任务名')))
+
+    // /* 修改任务  这个方法内部太臃肿，后期优化*/
+    // log(JSON.stringify(QlMod.EditQlCrons(s,qlDbArr, i, 'test.js')))
+
+    // /************* 环境变量相关****************/
+    // /* 获取所有环境变量 */
+    // console.log(JSON.stringify(await QlMod.GetQlAllEnvs(qlDbArr, i)));
+
+    /* 查找环境变量 
+      {
+        value: '',
+        _id: '08WF2V7ZkdSLi0BB',
+        created: 1681037010781,
+        status: 0,
+        timestamp: 'Sun Apr 16 2023 19:17:32 GMT+0800 (中国标准时间)',
+        position: 4.930380657138286e-22,
+        name: 'JD_R_WSCK',
+        remarks: '@@1681037015000@@'
+      }
+    */
+    console.log(JSON.stringify(await QlMod.SearchQlEnvs(qlDbArr, i, "JD_COOKIE")));
+
+    /* 添加环境变量  { status: true, data: '', msg: 'ok' } */
+    /*
+    console.log(JSON.stringify(QlMod.AddQlEnvs(qlDbArr, i, [{
+        "value": "ssssssssssssssss", //变量
+        "name": 'bbbbbbbbbb',   //变量名
+        "remarks": "cccccccccc" //备注
+    },
+    {
+        "value": "111", //变量
+        "name": '22',   //变量名
+        "remarks": "33" //备注
+    }])));
+    */
+
+    /* 更新环境变量 */
+    /*
+    console.log(
+        JSON.stringify(QlMod.EditQlEnvs(qlDbArr, i, {
+        "value": "111", //变量
+        "name": '22',   //变量名
+        "remarks": "33",//备注
+        "id": '1'  //id或_id
+    }))
+    );
+    */
+
+    /* 
+      启用和禁用环境变量  315为id 
+      { 
+        status: true, 
+        data: '', 
+        msg: 'ok' 
+      }
+    */
+    // log(JSON.stringify(QlMod.PutQlEnvs(qlDbArr, i, "enable", "315")))
+    // log(JSON.stringify(QlMod.PutQlEnvs(qlDbArr, i, "disable", "315")))
+
+    // /* 删除环境变量 */
+    // log(JSON.stringify(QlMod.DelQlEnvs(qlDbArr, i, "315")))
+
+    // /* 移动环境变量  5-1 */
+    // log(JSON.stringify(QlMod.MoveQlEnvs(qlDbArr, i, '315', { "fromIndex": 5, "toIndex": 1 })))
+
+
+    // /************* 订阅相关****************/
+    // /* 获取所有订阅任务 */
+    // log(JSON.stringify(QlMod.GetQlAllSubscribe(qlDbArr, i)))
+
+    // /* 运行、更新、启用禁用*/
+    // /* 
+    //  * 第三个参数 任务名字或脚本名关键词，方法内部会先读取所有任务-遍历所有任务找出包含关键词的任务
+    //  * 第四个参数 可选： stop停止 run运行 disable禁用 enable启用
+    //  * 第五个参数 布尔值，false 或不填 则会调用方法内部选择器(如果有多个，否则直接返回数据)让用户选择一个，true返回找到的第一个任务
+    //  */
+    // log(JSON.stringify(QlMod.PutQlSubscribe(s,qlDbArr, i, "关键词", "stop", false)))
+
+    // /* 获取订阅日志 */
+    // log(JSON.stringify(QlMod.GetQlSubscribeLogs(s,qlDbArr, i, '关键词', false)))
+
+    // /* 删除订阅 */
+    // log(JSON.stringify(QlMod.DelQlSubscribe(s,qlDbArr, i, '关键词', false)))
+
+
+    // 口令转url
+    // let { status, data } = await AmTool.jdCodeToUrl('');
+    // // 隐藏pin 保留前1 后 1
+    // let userName = AmTool.Masking('', 1, 1),
+}
---
+++ README.md
@@ -0,0 +1,47 @@
+# Bncr_plugin 简介
+
+自用的无界插件。
+
+# 安装与使用
+
+在红灯区同级目录创建一个文件夹，名字随意 如：自用插件，把当前仓库所有文件粘贴到自用插件下
+
+# 注意事项
+
+所有插件均无界超授可用。
+
+# 主要功能
+
+- 常规签到爬取.js，扫描某文档当天签到token。
+- 代理剩余次数.js，获取代理剩余数量。
+- 更换代理白名单.js，代理更换白名单，支持品易、巨量等。
+- 青龙定时任务迁移.js，从源青龙迁移定时任务到目标青龙。
+- 无线店铺签到清理.js，无线店铺签到清理替换并执行。
+- ck检测.js，ck有效性实时检测并通过奶酪棒绑定关系推送消息给用户。
+- JD_R_WSCK转换.js，调用nolan pro 转换api接口。
+- 青龙通知接口.js，通过青龙面板日志推送做一些事情。
+- Bncr定时任务功能插件.js，与Bncr 定时任务的交互的功能插件
+
+# 参与
+
+我们欢迎您在 Bncr_plugin 项目的 GitHub 上报告 issue。
+
+# 关于作者
+
+一个啥也不会的混子。
+
+# 友情链接
+- 无界官网
+  https://anmours.github.io/Bncr/#/
+
+- 红灯区插件
+  https://github.com/RedLightsDistrict/Bncr_plugins.git
+
+- onz3vyyds 
+  https://github.com/Yuheng0101/Bncr_plugins
+
+- 薛定谔的大灰机 
+  https://github.com/BigPlanes/Bncr_plugins
+
+- 小熊 
+  https://github.com/hsl6476/Bncr_plugins
